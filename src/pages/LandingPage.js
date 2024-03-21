8cdf95734c3df668280341ddeb6f77628d373283
nicoyee <nicholasyee159@gmail.com>
2024-03-21 13:43:15 +0800

    Worked on Landing Page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/LandingPage.css'
+import Navbar from '../components/Navbar';
+
+const LandingPage = () => {
+  return (
+
+    <Navbar />
+ 
+  );
+};
+
+export default LandingPage;
