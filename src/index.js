7e2b649cdbf2b2ec2e9fcbb35d401d7d4568dc2a
Ken Dimaymay <kendimaymay@gmail.com>
2024-04-02 23:38:06 +0800

    Updated login page and create acc page

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import LoginPage from './pages/LoginPage';
+import CreateAccountPage from './pages/CreateAccountPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
     {/* <App />  */}
-    <LoginPage />
+     {/* <LoginPage /> */}
+    <CreateAccountPage /> 
     
   </React.StrictMode>
 );
